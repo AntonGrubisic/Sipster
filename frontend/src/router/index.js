140471a1fc3221f631305e8eb77de2b1a8bdcb8a
martinez0466 <camilo93.martinez@gmail.com>
2025-11-04 13:30:15 +0100

    added wine-browser route to index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,11 @@ import {createRouter, createWebHistory} from 'vue-router'
 import LandingView from '../views/LandingView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
-import DashboardView from "@/views/DashboardView.vue";
+import QuizView from "@/views/QuizView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+import WineBrowserView from "@/views/WineBrowserView.vue";
 import CulinaryPairingsView from "@/views/CulinaryPairingsView.vue";
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL), // pass BASE_URL (recommended)
@@ -11,21 +14,12 @@ const router = createRouter({
         {path: '/', component: LandingView},
         {path: '/login', component: LoginView},
         {path: '/register', component: RegisterView},
-        {path: '/dashboard', component: DashboardView},
+        {path: '/quiz', component: QuizView, meta: {requiresAuth: true}},
+        {path: '/profile', component: ProfileView},
+        {path: "/wine-browser", name: "wine-browser", component: WineBrowserView},
         {path: "/culinary-pairings", name: "culinary-pairings", component: CulinaryPairingsView},
-    ]
-import QuizView from "@/views/QuizView.vue";
-import ProfileView from "@/views/ProfileView.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // pass BASE_URL (recommended)
-  routes: [
-    { path: '/', component: LandingView },
-    { path: '/login', component: LoginView },
-    { path: '/register', component: RegisterView },
-    { path: '/quiz', component: QuizView, meta: { requiresAuth: true } },
-    { path: '/profile', component: ProfileView }
-  ]
+    ]
 })
 
 export default router
