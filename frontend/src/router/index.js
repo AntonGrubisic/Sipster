08c0ecabaab69904e3fb7ecc31144fccad2c6bc7
Camilo <camilo93.martinez@gmail.com>
2025-11-05 14:47:20 +0100

    feature/culinary-pairings (#24)
    * Add initial Culinary Pairings view and update router + layout and formatted lines
    
    * culinary-pairings: add wineBrowser route/service + wire /api/wine-browser in app.js
    
    * added wine-browser route to index.js
    
    * modified app.js
    
    * fixed prefix on landingview
    
    * fixed landingview
    
    * Import WineBrowserView.vue från feature/wine-browser
    
    * added route to wines/pairings
    
    * fixed routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import LandingView from '../views/LandingView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import QuizView from "@/views/QuizView.vue";
 import ProfileView from "@/views/ProfileView.vue";
+import WineBrowserView from "@/views/WineBrowserView.vue";
+import CulinaryPairingsView from "@/views/CulinaryPairingsView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // pass BASE_URL (recommended)
-  routes: [
-    { path: '/', component: LandingView },
-    { path: '/login', component: LoginView },
-    { path: '/register', component: RegisterView },
-    { path: '/quiz', component: QuizView, meta: { requiresAuth: true } },
-    { path: '/profile', component: ProfileView }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL), // pass BASE_URL (recommended)
+    routes: [
+        {path: '/', component: LandingView},
+        {path: '/login', component: LoginView},
+        {path: '/register', component: RegisterView},
+        {path: '/quiz', component: QuizView, meta: {requiresAuth: true}},
+        {path: '/profile', component: ProfileView},
+        {path: '/wine-browser', component: WineBrowserView},
+        {path: '/culinary-pairings', component: CulinaryPairingsView},
+    ]
 })
 
 export default router
